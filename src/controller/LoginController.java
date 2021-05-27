7af0327ceb6dc26bd8640fd268bc268ab8169a42
BCadillac <1034204715@qq.com>
2021-05-27 11:28:10 +0800

    更改了注释和修改提示为中文

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -15,6 +15,7 @@ import pojo.Account;
 @Controller
 public class LoginController{
 	@RequestMapping(value="/",method=RequestMethod.GET)
+	//除了在controller里标注的页面，都会重定向访问登录页面。
 	public ModelAndView defaultEnterSystem()throws Exception{
 		return new ModelAndView("redirect:/login");
 	}
@@ -26,26 +27,21 @@ public class LoginController{
 	
 	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public ModelAndView handleLogin(Account inputAccount,HttpSession httpSession,ModelAndView mav) throws IOException{
-		AccountInfoDocDAO aidDAO=new AccountInfoDocDAO();
-		//ModelAndView mav=new ModelAndView();
-				
-		//check id existing
+		AccountInfoDocDAO aidDAO=new AccountInfoDocDAO();	
+		//检查用户ID是否存在
 		if(!aidDAO.isIdExisting(inputAccount.getId())){
 			mav.setViewName("login");
-			mav.addObject("failMessage","fail because the id is not existing");
+			mav.addObject("failMessage","用户ID不存在");
 		}else{
 			//根据id查找数据库得到密码
 			Account docAccount=aidDAO.getAccount(inputAccount.getId());
 			//数据库验证密码
 			if(docAccount.getPassword().equals(inputAccount.getPassword())){
-				//later should been changed to home page
-				//mav.setViewName("success");
-				//mav.addObject("message","login success");
 				httpSession.setAttribute("currentAccount", docAccount);
 				mav.setViewName("redirect:/home");
 			}else{
 				mav.setViewName("login");
-				mav.addObject("failMessage","fail because wrong password ");
+				mav.addObject("failMessage","密码错误");
 			}
 		}
 		return mav;
