71833635a7a1d7808f3440a214c64300429de5ca
BCadillac <1034204715@qq.com>
2021-06-07 10:19:33 +0800

    支持向前端返回返回json字符串

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSONObject;
 
 import DAO.GoodsInfoDocDAO;
 import pojo.Goods;
@@ -22,6 +25,17 @@ public class HomeController {
 		mav.setViewName("home");
 		mav.addObject("listOfSellingGoods",listOfSellingGoods);
 		return mav;
+	}
+	
+	@RequestMapping(value="/home",method=RequestMethod.POST, produces="text/html;charset=UTF-8")
+	@ResponseBody
+	public String getGoodsList() throws IOException{
+		GoodsInfoDocDAO gidDAO=new GoodsInfoDocDAO();
+		List<Goods> listOfSellingGoods = gidDAO.getAllSellingGoods();				
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("listOfSellingGoods", JSONObject.toJSON(listOfSellingGoods));
+		//System.out.println(jsonObject.toJSONString());
+		return jsonObject.toJSONString();
 	}
 	
 }
