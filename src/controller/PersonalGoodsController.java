4921c9ca8476a04da161da29bfc0d78e5820a38d
BCadillac <1034204715@qq.com>
2021-06-09 23:35:38 +0800

    增加了post方式返回数据

--- src/controller/PersonalGoodsController.java
+++ src/controller/PersonalGoodsController.java
@@ -8,7 +8,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSONObject;
 
 import DAO.GoodsInfoDocDAO;
 import pojo.Account;
@@ -25,4 +28,17 @@ public class PersonalGoodsController {
 		mav.addObject("listOfPersonalGoods",listOfPersonalGoods);
 		return mav;
 	}
+	
+	@RequestMapping(value="/personalGoods",method=RequestMethod.POST, produces="text/html;charset=UTF-8")
+	@ResponseBody
+	public String getGoodsList(HttpSession httpSession) throws IOException{
+		Account currentAccount=(Account)httpSession.getAttribute("currentAccount");
+		GoodsInfoDocDAO gidDAO=new GoodsInfoDocDAO();
+		List<Goods> listOfPersonalGoods = gidDAO.getPersonalGoods(currentAccount.getId());
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("listOfPersonalGoods", JSONObject.toJSON(listOfPersonalGoods));
+		System.out.println(jsonObject.toJSONString());
+		return jsonObject.toJSONString();
+	}
+	
 }
